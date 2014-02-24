0da5e1b8f5d966ab326bc4e2aa62f80eee9f3634
tracyone <tracyone@live.cn>
2014-02-24 22:41:10 +0800

    add some software like gimp,nautilus-action

--- software_install.sh
+++ software_install.sh
@@ -20,18 +20,35 @@ sudo add-apt-repository -y ppa:lainme/pidgin-lwqq
 echo "Update source...."
 sudo apt-get update
 
+echo "安装和配置 git svn等版本管理工具...."
+sudo apt-get install git gitk git-gui git-svn -y
+git config --global user.name "tracyone"
+git config --global user.email "tracyone@live.cn"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=86400'
+
+echo "安装goagent..."
+git clone https://github.com/goagent/goagent ./goagent
+sudo rm -rf /opt/goagent 
+sudo cp -a ./goagent /opt
+sudo chown -R $(whoami) /opt/goagent
+sudo chgrp -R $(whoami) /opt/goagent
+sudo apt-get -y install python-vte
+echo "配置goagent..."
+python /opt/goagent/server/uploader.zip
+sed -ie 's/^appid.*/appid = tracyone1989|tracyone1990/' /opt/goagent/local/proxy.ini
+
 echo "安装compiz特效管理..."
 sudo apt-get install compiz-plugins compiz-plugins-extra compizconfig-settings-manager -y
 
 echo "安装 qt4..."
 sudo apt-get install libqt4-dev libqt4-dbg libqt4-gui libqt4-sql qt4-dev-tools qt4-doc qt4-designer qt4-qtconfig -y
 
-echo "安装和配置 git...."
-sudo apt-get -y install git
-git config --global user.name "tracyone"
-git config --global user.email "tracyone@live.cn"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=86400'
+echo "安装gimp,Inkscape等图形软件..."
+sudo apt-get install gimp Inkscape Dia -y
+
+echo "安装java和java运行环境..."
+sudo apt-get install openjdk-7-jdk -y
 
 echo "卸载ibus然后安装 fcitx 输入法..."
 sudo apt-get -y remove ibus
@@ -40,13 +57,18 @@ sudo apt-get -y install fcitx fcitx-config-gtk fcitx-sunpinyin fcitx-googlepinyi
 echo "安装 pidgin ... "
 sudo apt-get install libpurple0 pidgin pidgin-lwqq -y 
 
+echo "安装goldendict..."
+sudo apt-get install goldendict goldendict-wordnet -y
+
+echo "安装nautils相关..."
+sudo apt-get -y install nautilus-open-terminal nautilus-actions
+
 echo "安装其它杂七杂八.."
 sudo apt-get -y install mercurial python-nautilus tortoisehg
-sudo apt-get -y install unrar p7zip-full zhcon xbacklight
+sudo apt-get -y install unrar p7zip-full zhcon xbacklight shutter wallch
 sudo apt-get install vlc -y
 sudo apt-get lm-sensors -y
 sudo apt-get hddtemp -y
-sudo apt-get -y install nautilus-open-terminal
 sudo apt-get -y install dconf-editor
 sudo apt-get -y install gparted
 
@@ -61,16 +83,6 @@ echo "待补充暂时不知道如何实现..."
 echo "恢复.zshrc"
 cp ./linux-config/.zshrc ~
 
-echo "安装goagent..."
-git clone https://github.com/goagent/goagent ./goagent
-sudo rm -rf /opt/goagent 
-sudo cp -a ./goagent /opt
-sudo chown -R $(whoami) /opt/goagent
-sudo chgrp -R $(whoami) /opt/goagent
-sudo apt-get -y install python-vte
-echo "配置goagent..."
-python /opt/goagent/server/uploader.zip
-sed -ie 's/^appid.*/appid = tracyone1989|tracyone1990/' /opt/goagent/local/proxy.ini
 
 echo "安装adobe flash player..."
 wget http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.341/install_flash_player_11_linux.x86_64.tar.gz
@@ -104,6 +116,9 @@ echo "安装gvim插件...可能需要比较长时间..."
 gvim -c :BundleInstall &
 
 echo "清除工作...."
+echo "卸载多余软件..."
+sudo apt-get purge gwibber -y
+sudo apt-get purge totem -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean
 sudo apt-get clean
