974bc72f576d26d2b095e3205f0209c4ff35d93f
tracyone <tracyone@live.cn>
2015-08-20 20:12:22 +0800

    默认使用缩放的方式来设置壁纸

--- chwall.sh
+++ chwall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # randomly chang background wallpaper in specified directory
-# optional argument:$1:The path of wallpaper
+# optional argument:The path of wallpaper
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
@@ -18,8 +18,10 @@ fi
 cd ${DIR_WALLPAPER}
 
 PIC_NAME=$(find . -regextype posix-egrep -regex '.*\.(jpg|png)' -type f | shuf -n1)
-echo -e "we are going to change background to ${DIR_WALLPAPER}/${PIC_NAME}"
+echo -e "we are going to change background to ${DIR_WALLPAPER}/${PIC_NAME#*./}"
 PIC_NAME=${DIR_WALLPAPER}/${PIC_NAME#*./}
+
+gsettings set org.gnome.desktop.background picture-options "zoom"
 
 gsettings set org.gnome.desktop.background picture-uri file://${PIC_NAME} || echo "Failed to change background Wallpapers" 
 #dconf write "/org/gnome/desktop/background/picture-uri" "file://${PIC_NAME}" ||  || echo "Failed to change background Wallpapers" 
