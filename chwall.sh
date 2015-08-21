ab6b48eff84f5f446b0b27adbed7e548afd07a7f
tracyone <tracyone@live.cn>
2015-08-21 23:17:03 +0800

    检测当前图片的选项防止反复调用设置图片的命令

--- chwall.sh
+++ chwall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # randomly chang background wallpaper in specified directory
 # optional argument:The path of wallpaper
+# options argument:scale
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
@@ -21,7 +22,12 @@ PIC_NAME=$(find . -regextype posix-egrep -regex '.*\.(jpg|png)' -type f | shuf -
 echo -e "we are going to change background to ${DIR_WALLPAPER}/${PIC_NAME#*./}"
 PIC_NAME=${DIR_WALLPAPER}/${PIC_NAME#*./}
 
-gsettings set org.gnome.desktop.background picture-options "zoom"
+cur_setting=$(gsettings get org.gnome.desktop.background picture-options)
+
+if [[ ! ${cur_setting} =~ .*zoom.* ]]; then
+	echo "Current Picture option is ${cur_setting},we change to zoom"
+	gsettings set org.gnome.desktop.background picture-options "zoom"
+fi
 
 gsettings set org.gnome.desktop.background picture-uri file://${PIC_NAME} || echo "Failed to change background Wallpapers" 
 #dconf write "/org/gnome/desktop/background/picture-uri" "file://${PIC_NAME}" ||  || echo "Failed to change background Wallpapers" 
