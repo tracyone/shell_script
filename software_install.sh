16afde49da48fbc1aa0e1a9a28db1aa713d24b8c
zxb <zxb@kfn.cn>
2015-12-26 23:05:51 +0800

    update安装脚本

--- software_install.sh
+++ software_install.sh
@@ -18,26 +18,23 @@ shopt -s expand_aliases
 read -p "请输入您的密码:" mypasswd
 alias sudo="echo "${mypasswd}" | sudo -S"
 
+if [[ -d ~/Work ]]; then
+	mkdir -p ~/Work
+fi
+cd ~/Work
+
 mkdir ./temp
 echo "添加仓库------------------------------------"
 sleep 3
 echo "添加 git 仓库..."
 sudo add-apt-repository -y ppa:git-core/ppa
-echo "添加 mercurial 仓库..."
-sudo add-apt-repository -y ppa:mercurial-ppa/releases
-echo "添加 tortoisehg 仓库..."
-sudo add-apt-repository -y ppa:tortoisehg-ppa/releases
 echo "添加 pidgin 仓库..."
 sudo add-apt-repository -y ppa:lainme/pidgin-lwqq 
-echo "添加 ubuntu-tweak 仓库..."
-sudo add-apt-repository -y ppa:tualatrix/ppa
 echo "添加 skype 仓库..."
 sudo add-apt-repository -y "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 echo "添加darktable 仓库..."
 sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release
 sudo add-apt-repository -y ppa:pmjdebruijn/darktable-release-plus
-echo "添加google drive 仓库..."
-sudo add-apt-repository -y sudo add-apt-repository ppa:thefanclub/grive-tools
 echo "Support i386 architecture"
 sudo dpkg --add-architecture i386
 
@@ -50,11 +47,6 @@ sudo apt-get dist-upgrade -y
 echo "安装和配置 git svn等版本管理工具...."
 sleep 3
 sudo apt-get install git gitk git-gui git-svn -y
-git config --global user.name "tracyone"
-git config --global user.email "tracyone@live.cn"
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=86400'
-git config --global core.editor vim
 
 echo "安装compiz特效管理..."
 sleep 3
@@ -98,7 +90,7 @@ sudo apt-get  install nautilus-open-terminal nautilus-actions -y
 
 echo "安装其它杂七杂八.."
 sleep 3
-sudo apt-get  install mercurial python-nautilus tortoisehg -y
+sudo apt-get  install python-nautilus -y
 sudo apt-get  install unrar p7zip-full zhcon xbacklight shutter wallch wmctrl -y
 sudo apt-get install vlc -y
 sudo apt-get lm-sensors -y
@@ -116,90 +108,68 @@ sudo apt-get install openbsd-inetd tftp-hpa tftpd-hpa -y
 sudo apt-get install nfs-kernel-server -y
 sudo apt-get install bison flex mtd-utils -y
 echo "设置tftp..."
-mkdir ~/tftpboot
-chmod 777 ~/tftpboot
-echo -e "RUN_DAEMON=\"yes\"" | sudo tee  /etc/default/tftpd-hpa
-echo -e "OPTIONS=\"-l -s -c /home/$(whoami)/tftpboot\"" | sudo tee -a /etc/default/tftpd-hpa
-echo -e "TFTP_USERNAME=\"root\"" | sudo tee -a /etc/default/tftpd-hpa
-echo -e "TFTP_DIRECTORY=\"/home/$(whoami)/tftpboot\"" | sudo tee -a /etc/default/tftpd-hpa
-echo -e "TFTP_ADDRESS=\"0.0.0.0:69\"" | sudo tee -a /etc/default/tftpd-hpa
-echo -e "TFTP_OPTIONS=\"--secure\"" | sudo tee -a /etc/default/tftpd-hpa
+mkdir ~/Work/tftpboot
+chmod 777 ~/Work/tftpboot
+echo -e "RUN_DAEMON=\"yes\"" > tftpd-hpa
+echo -e "OPTIONS=\"-l -s -c /home/$(whoami)/Work/tftpboot\"" >> tftpd-hpa
+echo -e "TFTP_USERNAME=\"root\"" >> tftpd-hpa
+echo -e "TFTP_DIRECTORY=\"/home/$(whoami)/Work/tftpboot\"" >> tftpd-hpa
+echo -e "TFTP_ADDRESS=\"0.0.0.0:69\"" >> tftpd-hpa
+echo -e "TFTP_OPTIONS=\"--secure\"" >> tftpd-hpa
+sudo mv tftpd-hpa /etc/default/tftpd-hpa
 sudo service tftpd-hpa restart
 echo "设置nfs..."
-mkdir ~/nfsroot
-chmod 777 ~/nfsroot
-echo -e "/home/$(whoami)/nfsroot *(rw,no_root_squash,no_all_squash,sync)" | sudo tee -a /etc/exports
+mkdir ~/Work/nfsroot
+chmod 777 ~/Work/nfsroot
+echo -e "/home/$(whoami)/Work/nfsroot *(rw,no_root_squash,no_all_squash,sync)" | sudo tee -a /etc/exports
 sudo exportfs -av
 sudo service nfs-kernel-server restart 
 
 if [[ ! -d "linux-config" ]];then
-   git clone https://github.com/tracyone/linux-config 
+   git clone https://github.com/tracyone/dotfile
 fi
-sudo ln -s /usr/bin/make /usr/bin/gmake
-sudo cp ./linux-config/*.desktop /usr/share/applications/
-
-echo "安装oh my zsh..."
-rm -rf ~/.oh-my-zsh
-sudo apt-get install zsh -y
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-echo "修改当前用户默认shell为zsh..."
-echo "待补充暂时不知道如何实现..."
-echo "恢复.zshrc"
-cp ./linux-config/.zshrc ~
-
+cd dotfile;./install.sh;cd -
 
 echo "Install steam..."
 sudo apt-get install steam steam-launcher -y
 
-echo "安装adobe flash player..."
-if [[ ! -f "install_flash_player_11_linux.x86_64.tar.gz" ]];then
-   wget http://fpdownload.macromedia.com/get/flashplayer/pdc/11.2.202.341/install_flash_player_11_linux.x86_64.tar.gz
-fi
-tar -xvf install_flash_player_11_linux.x86_64.tar.gz -C ./temp
-sudo cp -a temp/libflashplayer.so /usr/lib/mozilla/plugins
-sudo cp -a temp/usr/* /usr
-rm -rf ./temp/
+echo "Install chromium and Pepper Flash Player"
+sudo apt-get install chromium-browser
+sudo apt-get install pepperflashplugin-nonfree
+sudo update-pepperflashplugin-nonfree --install
 
-echo "下载专区------------------------------------"
 sudo apt-get install nautilus-dropbox -y
 
-sudo mkdir deb_file
-cd deb_file
-echo "下载搜狗输入法..."
-echo "下载dropbox ..."
-if [[ ${is_64} == "0" ]]; then
-	wget "http://pinyin.sogou.com/linux/download.php?f=linux&bit=32"
-	wget  "https://linux.dropbox.com/packages/ubuntu/dropbox_1.6.2_i386.deb" 
+echo "Install Sogou input ..."
+if [[ is_64 == 1 ]]; then
+	wget -c "http://pinyin.sogou.com/linux/download.php?f=linux&bit=64" sogoupinyin.deb
 else
-	wget "http://pinyin.sogou.com/linux/download.php?f=linux&bit=64"
-	wget  "https://linux.dropbox.com/packages/ubuntu/dropbox_1.6.2_amd64.deb" 
+	wget -c "http://pinyin.sogou.com/linux/download.php?f=linux&bit=32" sogoupinyin.deb
 fi
-sudo dpkg -i *.deb 
-cd ..
-rm -rf deb_file
+sudo dpkg -i sogoupinyin.deb
 
 
 echo "安装字体...需要很长时间请耐心等待..."
 if [[ ! -d "program_font" ]];then
    git clone https://github.com/tracyone/program_font
 fi
-sudo chmod -R a+x program_font/*
-sudo mkdir -p /usr/share/fonts/MyFonts
-mkdir ~/.fonts/
-cp ./program_font/* ~/.fonts/
-sudo cp ./program_font/* /usr/share/fonts/MyFonts
-sudo fc-cache -f -v
+if [[  $? -eq 0 ]]; then
+	sudo chmod -R a+x program_font/*
+	sudo mkdir -p /usr/share/fonts/MyFonts
+	mkdir ~/.fonts/
+	cp ./program_font/* ~/.fonts/
+	sudo cp ./program_font/* /usr/share/fonts/MyFonts
+	sudo fc-cache -f -v
+fi
 
-echo "安装 gvim..."
-sudo apt-get -y install vim-gtk cscope exuberant-ctags
-mkdir ~/.vim
-git clone https://github.com/tracyone/vim.git ~/.vim/vim
-cp ~/.vim/vim/.vimrc ~
-echo -e "Defaults\talways_set_home" | sudo tee -a /etc/sudoers
-sudo ln -s /home/tracyone/.vim /root/.vim
-sudo ln -s /home/tracyone/.vimrc /root/.vimrc
-echo "安装gvim插件...可能需要比较长时间..."
-gvim -c :BundleInstall &
+if [[ -d "vim_conf" ]]; then
+	git clone https://github/tracyone/vim vim_conf
+fi
+if [[ $? -eq 0 ]]; then
+	cd vim_conf ;./build_vim.sh all && gvim -c :BundleInstall &
+	cd -
+fi
+
 
 echo "避免ubuntu字体发虚..."
 sudo apt-get remove fonts-arphic-ukai ttf-wqy-zenhei fonts-arphic-uming -y
