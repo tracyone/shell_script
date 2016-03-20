04cc53bb2538bdaa0455eaa9df9ece8cb7d1b5d3
tracyone <tracyone@live.cn>
2016-03-20 19:00:46 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# install my shell script and dependence
+
+source ./common.sh
+
+sudo ln -sf $(pwd)/git_utils.sh /usr/local/bin/git_utils
+sudo mkdir -p /usr/local/bin/script_lib
+sudo ln -sf $(pwd)/common.sh /usr/local/bin/script_lib/
+
+if [[ $(GetOsType) == "LINUX" ]]; then
+	sudo apt-get install sox byzanz -y
+	git clone https://github.com/lolilolicon/FFcast2
+	cd FFcast2
+	make
+	sudo cp xrectsel /usr/bin
+	cd ..
+	rm -rf FFCAST2
+	sudo ln -sf $(pwd)/gif_record /usr/local/bin/gif_record
+fi
