18e18e574e056dfdae9f6998cbab037aa3c29c0b
tracyone <tracyone@live.cn>
2016-03-20 18:25:42 +0800

    add git_utils

---
+++ git_utils.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# author:tracyone
+# git utils
+# date:2016-03-20
+# usage:
+
+source common.sh
+
+# Function definition {{{
+# GitUtilsShowHelp:
+# argument:none
+function GitUtilsShowHelp()
+{
+   echo "Usage: `basename $0` [-d <tag name pattern>]"
+   exit 3;
+}
+
+# GitUtilsDeletTags
+# argument:tag_pattern
+function GitUtilsDeletTags()
+{
+	local tag=$(git tag)
+	local pattern="$1"
+	echo "pattern is ${pattern}"
+	if [[ ${tag} == "" ]]; then
+		echo -e "No tag found\n"
+		exit 0;
+	fi
+	for i in ${tag}; do
+		echo "i is $i"
+		if [[ $i =~ ${pattern} ]]; then
+			GetYn "delete remote and local tag:$i " "git push origin :refs/tags/$i" "git tag -d $i" 
+		fi
+	done
+	echo -e "Now we have following tags:"
+	git tag
+
+}
+# }}}
+
+if [[ $# -eq 0 ]]; then
+	GitUtilsShowHelp
+fi
+
+while getopts "d:" arg #选项后面的冒号表示该选项需要参数
+do
+		case $arg in
+			 d )
+				GitUtilsDeletTags "$OPTARG"
+				;;
+			* )
+				GitUtilsShowHelp
+				exit 1
+				;;
+		esac
+done
+
+case $1 in
+	"dtags" )
+		;;
+	* )
+		;;
+esac
+
+# vim: set ft=sh fdm=marker foldlevel=0 foldmarker&: 
